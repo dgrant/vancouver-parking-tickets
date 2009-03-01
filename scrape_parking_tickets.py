4ad82a68f10c60e2d38af29e84b699da5e621b16
David Grant <davidgrant@gmail.com>
2009-02-28 17:24:32 -0800

    refactored scraper and denormalized tables

--- scrape_parking_tickets.py
+++ scrape_parking_tickets.py
@@ -1,27 +1,66 @@
 from mechanize import Browser
+from BeautifulSoup import BeautifulSoup
 from lxml import html as HTML
+import datetime
 import pprint
+import re
+import time
+import sys
 
-ENDPOINT = "http://b2.caspio.com/dp.asp?appSession=67071265414986&RecordID=&PageID=2&PrevPageID=2&cpipage=2&CPIsortType=desc&CPIorderby=DE_dDate"
+recordID_regexp = re.compile("RecordID\=(\d*)")
+date_regexp = re.compile("\S*, (?P<month>\S*) (?P<date>\d*) '(?P<year>\d*)")
 
-class ParkingTicketScraper(object):
+ENDPOINT = "http://b2.caspio.com/dp.asp?AppKey=%s&RecordID=&PageID=2&PrevPageID=2&cpipage=%d&CPIsortType=desc&CPIorderby=DE_dDate"
+#DETAIL_ENDPOINT = "http://b2.caspio.com/dp.asp?AppKey=2b931000e4a5f0b1i7f3d6e0b6i&RecordID=%d&PageID=3&PrevPageID=2&cpipage=1&CPIsortType=desc&CPIorderBy=DE_dDate"
+
+class Scraper(object):
     def __init__(self):
         self.b = Browser()
         self.b.set_handle_robots(False)
-        self.b.open(ENDPOINT)
 
-    def run(self):
-        self.doc = HTML.fromstring(self.b.response().read(), self.b.geturl())
-        self._needle()
+        self.appkey = None
+        self.GetAppKey()
 
-    def _needle(self):
-        fields = ["Date", "Time", "License plate", "Make", "Street number", "Street", "Offence"]
-        count=1
-        for tr in self.doc.xpath("//table[1]/tr[@class='cbResultSetEvenRow'] | //table[1]/tr[@class='cbResultSetOddRow']"):
-            print "row", count 
+    def GetAppKey(self):
+        self.b.open("http://www.vancouversun.com/parking/basic-search.html")
+        html = self.b.response().read()
+        soup = BeautifulSoup(html)
+        for div in soup.findAll('div'):
+            if div.has_key('id') and div['id'] == 'cxkg':
+                self.appkey = div.findAll('a')[0]['href'].split('AppKey=')[1].split('&')[0]
+                break
+
+    def ScrapeTickets(self, page):
+        assert len(self.appkey) > 10
+        url = ENDPOINT % (self.appkey, page)
+        self.b.open(url)
+
+        doc = HTML.fromstring(self.b.response().read(), self.b.geturl())
+
+        tickets = []
+        fields = ["Date", "Time", "LicensePlate", "Make", "StreetNumber", "Street", "Offence", "RecordId"]
+        for tr in doc.xpath("//table[1]/tr[@class='cbResultSetEvenRow'] | //table[1]/tr[@class='cbResultSetOddRow']"):
             values = [td.text for td in tr.getchildren()[:7]]
-            pprint.pprint(zip(fields, values))
-            count += 1
+            td_details=tr.getchildren()[7]
+            # get the RecordID
+            values.append(re.search(recordID_regexp, td_details.xpath("a/@href")[0]).group(1))
 
-p = ParkingTicketScraper()
-p.run()
+            ticket = Ticket(zip(fields, values))
+            ticket.CleanUpRawTicket()
+            tickets.append(ticket)
+        return tickets
+
+class Ticket(dict):
+    def CleanUpRawTicket(self):
+        print self
+        date = datetime.datetime.strptime(self['Date'] + " " + self['Time'], "%a, %b %d '%y %H%M")
+        assert 2004 <= date.year < 2009
+        self['Date'] = date
+        del self['Time']
+
+s = Scraper()
+for i in xrange(1,54380):
+    start_time = time.time()
+    tickets = s.ScrapeTickets(int(sys.argv[1]))
+    pprint.pprint(tickets)
+    print "took", time.time() - start_time, "seconds"
--- vanparkingtickets/parkingtickets/models.py
+++ vanparkingtickets/parkingtickets/models.py
@@ -2,21 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Ticket(models.Model):
-    ticket_number = models.CharField(max_length=10)
+    recordID = models.CharField(max_length=10)
     date = models.DateTimeField()
-    plate = models.ForeignKey("Plate")
-    make = models.ForeignKey("Make")
+    plate models.CharField(max_length=10, unique=True)
+    make = models.CharField(max_length=20, unique=True)
     street_num = models.IntegerField()
     street_name = models.CharField(max_length=50)
-    offence = models.ForeignKey("Offence")
+    offence = models.CharField(max_length=100, unique=True)
     details = models.TextField()
     fine = models.DecimalField()
-
-class Plate(models.Model):
-    plate = models.CharField(max_length=10, unique=True)
-
-class Make(models.Model):
-    name = models.CharField(max_length=20, unique=True)
-
-class Offence(models.Model):
-    name = models.CharField(max_length=100, unique=True)
